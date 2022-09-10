f1f3a2e682225a380630bdd2c6b0bbab791cf100
JonOfScience <jdcrofts31@gmail.com>
2022-09-10 20:01:22 +0100

    Further refactoring and cleaning test_deserialiser

--- tests/test_Deserialiser.py
+++ tests/test_Deserialiser.py
@@ -7,6 +7,7 @@ from lib.services.io_service import Deserialiser
 class TestGivenAnUndefinedDeserialiser:
     """Test operations for a serialiser without a defined format"""
     def test_empty_instantiation_will_throw(self):
+        """Instantiation requires a format to be specified """
         with pytest.raises(Exception) as e_info:
             Deserialiser()
         assert e_info.type == TypeError
@@ -15,12 +16,14 @@ class TestGivenAnUndefinedDeserialiser:
 class TestGivenAValidDeserialiserFormat:
     """Instantiation for correctly defined deserialisers"""
     def test_a_deserialiser_in_json_format_can_be_constructed(self):
+        """JSON is a recognised format"""
         assert Deserialiser(DataFormat.JSON)
 
 
 class TestGivenAnInvalidDeserialiserFormat:
     """Misidentified formats do not throw on instantiation, but when deserialise is called"""
     def test_deserialiser_will_throw_on_deserialise(self):
+        """Deserialiser will throw as it cannot find the format for the specified string"""
         with pytest.raises(Exception) as e_info:
             Deserialiser("ThisIsNotAFormat").deserialise('{"Any": "String}')
         assert e_info.type == KeyError
@@ -40,16 +43,16 @@ class Test_GivenADeserialiserInJSONFormat:
         mock_deserialise.assert_called_once()
 
     def test_cm_ic_7_a_valid_string_will_deserialise(self):
-        # State test
-        # Integration Test: Deserialiser <- to/from -> JSON_Deserialiser
+        """State test
+        Integration Test: Deserialiser <- to/from -> JSON_Deserialiser"""
         json_deserialiser = Deserialiser(DataFormat.JSON)
         teststring = '{"A": "B", "C": 1}'
         deserialised_object = json_deserialiser.deserialise(teststring)
         assert deserialised_object == {"A": "B", "C": 1}
 
     def test_a_none_input_returns_an_empty_dict(self):
-        # State test
-        # Integration Test: Deserialiser <- to/from -> JSON_Deserialiser
+        """State test
+        Integration Test: Deserialiser <- to/from -> JSON_Deserialiser"""
         json_deserialiser = Deserialiser(DataFormat.JSON)
         teststring = None
         deserialised_object = json_deserialiser.deserialise(teststring)
