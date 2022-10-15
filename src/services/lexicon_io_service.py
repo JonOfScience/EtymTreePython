a2152df8e6e1d3d832ae47b70c5f44926dd58206
JonOfScience <jdcrofts31@gmail.com>
2022-10-15 20:01:30 +0100

    Lexicon.load_from refactored to use LexiconIOService. Tests pass.

--- src/services/lexicon_io_service.py
+++ src/services/lexicon_io_service.py
@@ -15,8 +15,19 @@ class LexiconIOService:
             raise ValueError("Either IOService or a DataFormat must not be None.")
 
     def store_to(self, filename: str, word_data: Sequence[dict]):
-        """Serialiser Word data and pass to I/O servoce for storage"""
+        """Serialise Word data and pass to I/O service for storage"""
         output_data = []
         for word_dict in word_data:
             output_data.append(self._io_service.serialise_obj_to_string(word_dict) + "\n")
         self._io_service.store(filename, output_data)
+
+    def load_from(self, filename: str):
+        """Read data from storage using I/O service and clean"""
+        input_data = []
+        input_string = self._io_service.read(filename)
+        input_strings = input_string.split(sep="\n")
+        for serialised_string in input_strings:
+            if serialised_string:
+                deserialised_data = self._io_service.deserialise_string_to_obj(serialised_string)
+                input_data.append(deserialised_data)
+        return input_data
