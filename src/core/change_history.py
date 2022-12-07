b1a0e46a425c308a19524907af82f737373f5070
JonOfScience <jdcrofts31@gmail.com>
2022-12-07 16:51:43 +0000

    get_all_items and retrieve_export_data_for added to LexiconChangeHistory

--- src/core/change_history.py
+++ src/core/change_history.py
@@ -6,7 +6,17 @@ from core.change_history_item import ChangeHistoryItem
 class LexiconChangeHistory:
     """Repository for ChangeHistoryItems."""
     def __init__(self) -> None:
-        self._items = Sequence[ChangeHistoryItem]
+        self._items: Sequence[ChangeHistoryItem] = []
+
+    def get_all_items(self) -> Sequence[ChangeHistoryItem]:
+        """List all ChangeHistoryItems currently registered in the History"""
+        return self._items
+
+    def retrieve_export_data_for(self, items: Sequence[ChangeHistoryItem] = None) -> Sequence[dict]:
+        """Translate internal data of ChangeHistoryItem instances into serialisable format."""
+        if items is not None:
+            return [x.data_for_export() for x in items]
+        return [x.data_for_export() for x in self.get_all_items()]
 
     # def store_to(self, filename: str):
     #     """Serialise and store Word entries locally"""
